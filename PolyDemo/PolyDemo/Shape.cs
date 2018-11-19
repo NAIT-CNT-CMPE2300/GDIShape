f49bca8830dce835ef97e3bf0897a498c6629cda
AJ Armstrong <aja@clanarmstrong.com>
2018-11-19 07:50:47 -0700

    Initial import

---
+++ PolyDemo/PolyDemo/Shape.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using GDIDrawer;
+
+namespace PolyDemo
+{
+    class Shape : IComparable
+    {
+        #region Static Properties
+        public static int CanvasWidth { set; get; }
+        public static int CanvasHeight { set; get; }
+        public static int MaxSpeed { set; get; }
+        private static Random Random { get; }
+        #endregion
+
+        #region Properties
+        public PointF Location { set; get; }
+        public PointF Velocity { set; get; }
+        public Color Color { set; get; }
+        #endregion
+
+        #region Constructors
+        static Shape()
+        {
+            //Bases on scaled width, height, etc.
+            CanvasWidth = 800;
+            CanvasHeight = 600;
+            MaxSpeed = 5;
+            Random = new Random();
+        }
+
+        public Shape() //Default ctor Randomificates
+        {
+            Location = new PointF(Random.Next(CanvasWidth),
+                Random.Next(CanvasHeight));
+            Velocity = new PointF((float)Random.NextDouble() * 2 * MaxSpeed - MaxSpeed, 
+                (float)Random.NextDouble() * 2 * MaxSpeed - MaxSpeed);
+            Color = RandColor.GetColor();
+        }
+
+        public Shape(bool b):this() //Dummy overload for the ICA
+        {
+            //Could do this with enums and case statements,
+            //but anonymous initializer lists are fun!
+            Color = new List<Color> { Color.Red, Color.Blue,
+                Color.Green, Color.Yellow, Color.Purple}[Random.Next(5)];
+        }
+        #endregion
+
+        #region Methods
+        public static void SetCanvasProperties(CDrawer canvas)
+        {
+            //Reset static canvas size based on the sample I just got
+            CanvasWidth = canvas.ScaledWidth;
+            CanvasHeight = canvas.ScaledHeight;
+        }
+
+        //Reset position+=velocity, corrected with bounce
+        //Returns new position
+        //TODO:  Eww.  PointFs aren't editable.  Redo with 
+        //       a tuple or something.
+        //NB! Assumes 'Location' is CENTER of shape.
+        //Remember to paint with Centered methods
+        public PointF Move() 
+        {
+            Location = new PointF(Location.X + Velocity.X,
+                Location.Y + Velocity.Y);
+
+            //Check if now out of bounds, correct
+            if(Location.X < 0) //Left edge
+            {
+                Location = new PointF(0, Location.Y);
+                Velocity = new PointF(-Velocity.X, Velocity.Y); //Boing!
+            }
+            if (Location.X  > CanvasWidth) //Right edge
+            {
+                Location = new PointF(CanvasWidth, Location.Y);
+                Velocity = new PointF(-Velocity.X, Velocity.Y); //Boing!
+            }
+
+            //Check if now out of bounds, correct
+            if (Location.Y < 0) //Top edge
+            {
+                Location = new PointF(Location.X, 0);
+                Velocity = new PointF(Velocity.X, -Velocity.Y); //Boing!
+            }
+            if (Location.Y > CanvasHeight) //Bottom edge
+            {
+                Location = new PointF(Location.X, CanvasHeight);
+                Velocity = new PointF(Velocity.X, -Velocity.Y); //Boing!
+            }
+
+            return Location;
+        }
+
+        public void Render(GDIDrawer.CDrawer canvas)
+        {
+            canvas.AddCenteredEllipse((int)Location.X, (int)Location.Y,
+                10, 10, this.Color);
+
+        }
+        #endregion
+
+        #region Overrides
+        //Override base class methods
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Shape that))
+                return false;
+            return that.Color == this.Color
+                && that.Location == this.Location
+                && that.Velocity == this.Velocity;
+        }
+
+        public override int GetHashCode()
+        {
+            //Fancy Tuple trick
+            return new Tuple<Color, PointF, PointF>(
+                Color,Location,Velocity).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} Shape at ({Location.X},{Location.Y})";
+        }
+        #endregion
+
+        #region Interfaces
+        //Required for IComparable
+        //Sort by color.
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Shape that))
+                throw new ArgumentException(
+                    "Invalid object passed to Shape.CompareTo()");
+            return this.Color.ToArgb() - that.Color.ToArgb();
+        }
+        #endregion
+    }
+}
---
+++ PolyDemo/PolyDemo/frmMain.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using GDIDrawer;
+
+
+namespace PolyDemo
+{
+    public partial class frmMain : Form
+    {
+        private static CDrawer canvas = null;
+        private Thread Animator = null;
+        private List<Shape> Shapes = null;
+        private List<Shape> newShapes = null;
+        private const int canvWidth = 800;
+        private const int canvHeight = 600;
+        private const int delay = 50; //ms of sleep in animate cycle
+
+        static frmMain()
+        {
+            canvas = new CDrawer(canvWidth, canvHeight);
+        }
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //The set of Shapes that I will animate (empty at first)
+            //My GUI thread will only add Shapes and never remove
+            //or modify them, so thread safety should be good.
+            //Otherwise, should probably add some locking and or
+            //set up more sophisticated inter-thread communication.
+            Shapes = new List<Shape>(10);
+
+            //Start animation thread
+            Animator = new Thread(new ParameterizedThreadStart(Animate));
+            Animator.IsBackground = true;
+            Animator.Start(Shapes);
+
+            //set up my event handlers
+            if (!(canvas is null))
+            {
+                canvas.MouseLeftClick += new GDIDrawerMouseEvent(HandleMouse);
+                canvas.KeyboardEvent += new GDIDrawerKeyEvent(HandleKey);
+            }
+        }
+
+        //NB: if button pushed again, we'll just add 10 more Shapes.
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //If timer is running, it could goof with my list.
+            tmrAddShapes.Enabled = false;
+
+            //Make sure I got a canvas
+            if(canvas is null || canvas.DrawerWindowSize.IsEmpty) canvas = new CDrawer(canvWidth, canvHeight);
+
+            //Set my Shapes to bounce on that size canvas
+            Shape.SetCanvasProperties(canvas);
+
+            //Create my list of 10 Shapes, obeying the rules for 
+            //resolving conflicts
+            newShapes = new List<Shape>(10);
+            while(newShapes.Count < 10)
+            {
+                Shape shape = new Shape(true);
+                if (!newShapes.Contains(shape)) newShapes.Add(shape);
+            }
+            //Sort them
+            newShapes.Sort();
+
+            //Start adding them to the animation by letting my timer go.
+            tmrAddShapes.Enabled = true;
+        }
+
+        private void Animate(object ListOShapes)
+        {
+            //Quick and dirty erase everything (could get fancy and overwrite
+            //existing Shapes with black just before move and redraw, but that's
+            //probably premature optimization).
+            if(!(ListOShapes is List<Shape> Shapes)) return; //What is this thing?
+
+            while (true)//Forevs, unless parent thread goes away.
+            {
+                canvas.Clear();
+                //My kingdom for a foreach with mutable list elements
+                lock (Shapes)
+                {//Added because my event handlers could crossthread
+                    for (int i = 0; i < Shapes.Count; ++i)
+                    {
+                        Shape b = Shapes[i];
+                        //Move first.  That makes sure everything is on the canvas.
+                        b.Move();//This changes b! no other threads should be changing b, or I need to do more.
+                        b.Render(canvas);
+                    }
+                }
+                //Show the pretty thingees
+                canvas.Render();
+                Thread.Sleep(delay);
+            }
+        }
+
+        private void tmrAddShapes_Tick(object sender, EventArgs e)
+        {
+            if (newShapes.Count > 0)
+            {
+                //What clown didn't have RemoveAt return the item removed?
+                Shape shape = newShapes[0];
+                newShapes.RemoveAt(0);
+                Shapes.Add(shape);
+            }
+            else //list empty.  no real point in ticking.
+                tmrAddShapes.Enabled = false;
+        }
+
+        //Event Handler for Mouse things
+        private void HandleMouse(System.Drawing.Point pos, GDIDrawer.CDrawer drawer)
+        {
+            //add a Shape when and where you click
+            lock(Shapes) //The other thread might be using it
+            {
+                Shape b = new Shape(true);
+                b.Location = new PointF(pos.X, pos.Y);
+                Shapes.Add(b);
+            }
+        }
+
+        //Event Handler for Keyboard things 
+        private void HandleKey(bool bIsDown, Keys keycode, GDIDrawer.CDrawer drawer)
+        {
+            // Don't do anything until they release the key (the event fires multiples).
+            if (bIsDown) return;
+            switch (keycode)
+            {
+                case Keys.D:
+                    //Delete a Shape
+                    lock (Shapes)
+                        if (Shapes.Count > 0) Shapes.RemoveAt(0);
+                    break;
+                case Keys.A:
+                    //Add a Shape
+                    lock (Shapes)
+                        Shapes.Add(new Shape(true));
+                    break;
+            }
+        }
+
+    }
+}
